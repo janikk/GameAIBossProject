80697de7ed62ab052447a75d0cee14fa24623669
cjsloat97 <cjsloat97@gmail.com>
2018-12-03 04:21:07 -0500

    Add base project and bullet logic

---
+++ Final Project/Assets/Scenes/BulletHoming.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletHoming : MonoBehaviour {
+
+	GameObject target;
+
+	Vector3 dir;
+
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find("Player");
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		dir = (target.transform.position - transform.position).normalized;
+
+		rb.velocity = new Vector2(dir.x * 65, dir.y * 65); 
+	}
+}
---
+++ Final Project/Assets/Scenes/shooting.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class shooting : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+	public GameObject homingPrefab;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Fire()
+	{
+		Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		// Create the Bullet from the Bullet Prefab
+		var bullet = (GameObject)Instantiate(
+				bulletPrefab,
+				mouse,
+				Quaternion.identity);
+
+		// Add velocity to the bullet
+		bullet.GetComponent<Rigidbody2D>().velocity = Vector2.right * 65;
+
+		// Destroy the bullet after 2 seconds
+		Destroy(bullet, 2.0f);
+	}
+
+	void FireHoming()
+	{
+		Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		// Create the Bullet from the Bullet Prefab
+		var bullet = (GameObject)Instantiate(
+				homingPrefab,
+				mouse,
+				Quaternion.identity);
+
+		// Add velocity to the bullet
+
+		Destroy(bullet, 2.0f);
+		// Destroy the bullet after 2 seconds
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			Fire();
+		}
+		else if (Input.GetMouseButtonDown(1))
+		{
+			FireHoming();
+		}
+	}
+}
