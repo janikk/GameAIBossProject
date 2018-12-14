f235b72e1f416d4955b91ea0d77efa6afed0753f
upscalerat <heather.siobhans@gmail.com>
2018-12-14 01:23:28 -0500

    added state machine to boss, still need to add rush attack

--- Final Project/Assets/BossAction.cs
+++ Final Project/Assets/BossAction.cs
@@ -1,24 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class BossAction : MonoBehaviour
 {
-
     public float moveSpeed = 5f;
     public float rotSpeed = 100f;
-
-    private bool attackgoing = false;
     private bool isWandering = false;
     private bool isRotatingLeft = false;
     private bool isRotatingRight = false;
-
     // Use this for initialization
     void Start()
     {
-
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -27,17 +20,17 @@ public class BossAction : MonoBehaviour
         //If not wandering....
         if (!isWandering)
         {
-            int attacktype = Random.Range(1,4);
-            
-            print(attacktype);
-            if(!attackgoing){
-                attackgoing = true;
-                StartCoroutine(attack(attacktype));
+            //What kind of movement?
+            float moveType = Random.Range(0f, 1f);
+            if (moveType <= .15f)
+            {
+                StartCoroutine(Rush());
             }
-           // while(attackgoing){}
-            StartCoroutine(Wander());
+            else
+            {
+                StartCoroutine(Wander());
+            }
         }
-
         if (isRotatingRight)
         {
             //Keep rotating to the right until false
@@ -49,52 +42,16 @@ public class BossAction : MonoBehaviour
             transform.Rotate(Vector3.forward * Time.deltaTime * -rotSpeed);
         }
     }
-
-    IEnumerator attack(int attacktype)
-    {
-        BulletSpawnerBoss attacking = transform.GetComponent<BulletSpawnerBoss>();
-        if(attacktype == 1){
-            attacking.FireNormal();
-            yield return new WaitForSeconds(1);
-            attacking.FireNormal();
-            yield return new WaitForSeconds(.5f);
-            attacking.FireNormal();
-            yield return new WaitForSeconds(.5f);
-            attacking.FireNormal();
-            yield return new WaitForSeconds(3);
-        }
-        else if(attacktype == 2){
-            attacking.FireHoming();
-            yield return new WaitForSeconds(1);
-            attacking.FireHoming();
-            yield return new WaitForSeconds(4);
-        }
-        else{
-            attacking.FireSplit();
-            yield return new WaitForSeconds(2);
-            attacking.FireSplit();
-            yield return new WaitForSeconds(2);
-            attacking.FireSplit();
-            yield return new WaitForSeconds(3);
-        }
-
-        //Allows us to call this again
-        attackgoing = false;
-    }
-
     IEnumerator Wander()
     {
         //Some numbers
         int rotTime = Random.Range(1, 3);
         int wait = Random.Range(1, 4);
         int rotLorR = Random.Range(1, 3);
-
         //Set wandering to true so update doesn't keep calling this
         isWandering = true;
-
         //Wait for a few seconds- keep going forward
         yield return new WaitForSeconds(wait);
-
         //Which way are we turning?
         if (rotLorR == 1)
         {
@@ -103,7 +60,6 @@ public class BossAction : MonoBehaviour
             yield return new WaitForSeconds(rotTime);
             isRotatingRight = false;
         }
-
         if (rotLorR == 2)
         {
             //Rotate for rotTime seconds (but left)
@@ -111,8 +67,19 @@ public class BossAction : MonoBehaviour
             yield return new WaitForSeconds(rotTime);
             isRotatingLeft = false;
         }
-
         //Allows us to call this again
+        isWandering = false;
+    }
+
+    IEnumerator Rush()
+    {
+        //How long to rush for
+        int rush = Random.Range(1, 4);
+        //Is it wandering? No. Will this make it work? yeah.
+        isWandering = true;
+        //PURSUE ALGORITHM HERE
+        yield return new WaitForSeconds(rush);
+        //back to the same old same old
         isWandering = false;
     }
 }
---
+++ Final Project/Assets/BossStates.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossStates : MonoBehaviour {
+
+    private int HP;
+
+    private bool attack = false;
+
+    enum AttackState
+    {
+        STRAIGHT,
+        HOMING
+    }
+
+    AttackState AS;
+
+	// Use this for initialization
+	void Start () {
+        AS = AttackState.STRAIGHT;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Get health
+        HP = int.Parse(GameObject.Find("Boss_HP").GetComponent<Text>().text);
+        //Generate number to determine attack
+        float attackType = Random.Range(0f, 1f);
+
+        //If attacking is false, attack
+        if (!attack)
+        {
+            //Attack difficulty determined by health
+            if (AS == AttackState.STRAIGHT)
+            {
+                StartCoroutine(StraightAttack(attackType));
+            }
+            else if (AS == AttackState.HOMING)
+            {
+                StartCoroutine(HomingAttack(attackType));
+            }
+        }
+        if (HP < 30)
+        {
+            AS = AttackState.HOMING;
+        }
+	}
+
+    IEnumerator StraightAttack(float attackType)
+    {
+        //Attacking- don't want to have a barrage of bullets
+        attack = true;
+        BulletSpawnerBoss attacking = transform.GetComponent<BulletSpawnerBoss>();
+        //Don't attack 50% of the time
+        if (attackType >= 0.5f)
+        {
+            Debug.Log("30+ health, don't attack");
+            yield return new WaitForSeconds(2);
+        }
+        //Straight shot 25% of the time
+        else if (attackType >= .25f)
+        {
+            Debug.Log("30+ health, straight shot");
+            attacking.FireNormal();
+            yield return new WaitForSeconds(2);
+        }
+        //Straight homing shot 13% of the time
+        else if (attackType >= .12f)
+        {
+            Debug.Log("30+ health, homing shot");
+            attacking.FireHoming();
+            yield return new WaitForSeconds(2);
+        }
+        //Scattered homing shot 12% of the time
+        else
+        {
+            Debug.Log("30+ health, split homing");
+            attacking.FireSplit();
+            yield return new WaitForSeconds(2);
+        }
+        //No longer attacking
+        attack = false;
+    }
+
+    IEnumerator HomingAttack(float attackType)
+    {
+        //Attacking- don't want to have a barrage of bullets
+        attack = true;
+        BulletSpawnerBoss attacking = transform.GetComponent<BulletSpawnerBoss>();
+        //Scattered homing shot 25% of the time
+        if (attackType >= 0.75f)
+        {
+            Debug.Log("<30 health, split homing");
+            attacking.FireSplit();
+            yield return new WaitForSeconds(1);
+        }
+        //Straight homing shot 25% of the time
+        else if (attackType >= .5f)
+        {
+            Debug.Log("<30 health, homing shot");
+            attacking.FireHoming();
+            yield return new WaitForSeconds(1);
+        }
+        //Summon minions 30% of the time
+        else if (attackType >= .2f)
+        {
+            Debug.Log("<30 health, summon minions");
+            //attacking.SummonMinions();
+            yield return new WaitForSeconds(1);
+        }
+        //Straight shot 20% of the time
+        else
+        {
+            Debug.Log("<30 health, straight shot");
+            attacking.FireNormal();
+            yield return new WaitForSeconds(1);
+        }
+        attack = false;
+    }
+}
