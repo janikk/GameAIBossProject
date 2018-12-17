6a10f3474dd11091da4009a6ce9cae7203a7e2e1
janikk <janikk@rpi.edu>
2018-12-16 22:24:03 -0500

    Done and submitted

--- Final Project/Assets/Manager.cs
+++ Final Project/Assets/Manager.cs
@@ -1,16 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Manager : MonoBehaviour {
 
+    private GameObject Boss_HP;
+	private GameObject Player_HP;
+	public GameObject endScreen;
 	// Use this for initialization
 	void Start () {
-		
+		Boss_HP = GameObject.Find("Boss_HP");
+		Player_HP = GameObject.Find("Character_HP");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(int.Parse(Player_HP.GetComponent<Text>().text) < 0){
+			endScreen.GetComponentInChildren<Text>().text = "Game Over\nPress Escape to close the game";
+			Time.timeScale = 0.0f;
+		}
+		else if(int.Parse(Boss_HP.GetComponent<Text>().text) < 0) {
+			endScreen.GetComponentInChildren<Text>().text = "You Win\nPress Escape to close the game";
+			Time.timeScale = 0.0f;
+		} 
+		if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
 	}
 }
---
+++ README.txt
@@ -0,0 +1,3 @@
+Project by Kalvin Janik, Heather Sheridan, CJ Sloat, Matthew Carlson
+A and D to move left and right. Space to jump. Click to shoot.
+We each tried taking on the various elements of the AI including the boss's states, movement, attack choices, and the flocking of the minions.
